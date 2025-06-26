83c0e4c813c347675c42a94b9d8c9ecad64bf361
Divya <pai.divya03@gmail.com>
2025-06-26 09:32:52 +0530

    CodeRabbit suggestions

--- src/main/java/co/in/vollen/purrsonal/entity/User.java
+++ src/main/java/co/in/vollen/purrsonal/entity/User.java
@@ -10,7 +10,6 @@ import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
